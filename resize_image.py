ef74f530a9cfb6fd17ce8ca0a7cf8edd86261a1b
YunhoChoi <choiking10@gmail.com>
2020-05-03 16:47:36 +0900

    image resize script

---
+++ resize_image.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+import os
+
+for filename in os.listdir("test"):
+    if not(filename.endswith("png") or filename.endswith("jpg") or filename.endswith("jpeg")):
+        continue
+    print("file :" + filename)
+
+    image = Image.open(os.path.join("test", filename))
+    width = image.size[0]
+    height = image.size[1]
+
+    aspect = width / float(height)
+
+    ideal_width = 1045
+    ideal_height = 555
+
+    ideal_aspect = ideal_width / float(ideal_height)
+
+    thumb = image.resize((ideal_width, ideal_height), Image.ANTIALIAS)
+    thumb.save('output/'+filename)
